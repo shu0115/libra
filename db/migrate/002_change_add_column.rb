ad5a9988ad2e24a784f6214936dfe606ad091dc2
Shun Matsumoto <raisondetre0115@gmail.com>
2010-10-03 00:14:04 +0900

    update

--- db/migrate/002_change_add_column.rb
+++ db/migrate/002_change_add_column.rb
@@ -1,7 +1,7 @@
 class ChangeAddColumn < ActiveRecord::Migration
   def self.up
     add_column :items, :cycle_mode, :string
-    change_column :items, :cycle, :integer
+#    change_column :items, :cycle, :integer
   end
 
   def self.down
